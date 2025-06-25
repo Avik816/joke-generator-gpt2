07478b1519e6fcee843d76c31a863ddb24f8b26f
Avik816 <manikchakrabarti745@gmail.com>
2025-06-26 03:19:38 +0530

    Update modules

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from .raw_dataset_related.raw_dataset_downloader import download_dataset
 from .raw_dataset_related.raw_dataset_maker import full_raw_dataset_creation
 from .preprocessing.data_preprocessing import cleaning_dataset
+from .data_for_model.model_data_1 import data_splitting
 
-__sub_modules__ = ['download_dataset', 'full_raw_dataset_creation', 'cleaning_dataset']
+__sub_modules__ = ['download_dataset', 'full_raw_dataset_creation', 'cleaning_dataset', 'data_splitting']
