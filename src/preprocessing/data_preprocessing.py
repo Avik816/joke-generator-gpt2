9646f79fa58d4097099602f610731dda40c61384
Avik Chakraborty <manikchakrabarti745@gmail.com>
2025-06-25 21:48:55 +0530

    Update preprocessing.py

--- src/preprocessing/data_preprocessing.py
+++ src/preprocessing/data_preprocessing.py
@@ -1,34 +1,11 @@
 import polars
 import re
-import html
 
 
 def checking_for_url(text):
     url_pattern = re.compile(r'https?://|www\.|\.com|\.net|\.org|\.io|\.gov|\.in|\.me|\.co', re.IGNORECASE)
 
     return bool(url_pattern.search(str(text)))
-
-"""def deleting_quotes(text):
-    return (
-        text.replace('“', '"').replace('”', '"').
-        replace('‘', "'").replace('’', "'")
-    )
-
-def removing_emojis(text):
-    emoji_pattern = re.compile(
-        "["
-        "\U0001F600-\U0001F64F"  # Emoticons
-        "\U0001F300-\U0001F5FF"  # Symbols & pictographs
-        "\U0001F680-\U0001F6FF"  # Transport & map symbols
-        "\U0001F1E0-\U0001F1FF"  # Flags
-        "\U00002700-\U000027BF"  # Dingbats
-        "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-        "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-        "\U00002600-\U000026FF"  # Miscellaneous Symbols
-        "\U00002500-\U00002BEF"  # CJK (some emoji/symbol overlap)
-        "]+", flags=re.UNICODE)
-    
-    return emoji_pattern.sub(r'', str(text))
 
 def checking_for_html_entities(text):
     html_entity_pattern = re.compile(r"&(gt|lt|amp|quot|apos);", re.IGNORECASE)
@@ -38,15 +15,59 @@ def checking_for_html_entities(text):
 def checking_for_square_brackets(text):
     return bool(re.search(r"\[|\]", str(text)))
 
-def clean_text(text):
+def deleting_quotes(text):
+    return (
+        text.replace('“', '"').replace('”', '"').
+        replace('‘', "'").replace('’', "'")
+    )
+
+def removing_emojis(text):    
+    emoji_pattern = re.compile(
+        r"["
+        "\U0001F600-\U0001F64F"  # Emoticons (😀–😿)
+        "\U0001F300-\U0001F5FF"  # Symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # Transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # Regional flags
+        "\U00002500-\U00002BEF"  # Chinese/Japanese/Korean
+        "\U00002700-\U000027BF"  # Dingbats
+        "\U0001F900-\U0001F9FF"  # Supplemental symbols
+        "\U0001FA70-\U0001FAFF"  # Extended pictographs
+        "\U000024C2-\U0001F251"  # Enclosed characters
+        "\uFF00-\uFFEF"          # Full-width ASCII variants
+        "\u2600-\u26FF"          # Misc symbols
+        "\u2000-\u206F"          # General punctuation
+        "\u00A0-\u00FF"          # Latin-1 Supplement
+        "\u0100-\u017F"          # Latin Extended-A
+        "\u0180-\u024F"          # Latin Extended-B
+        "]+", flags=re.UNICODE
+    )
+    
+    return emoji_pattern.sub('', str(text))
+
+def removing_kaoemoji_patterns(text):
+    kaomoji_face_pattern = re.compile(r"\([\s\S]{3,20}?\)")
+
+    return kaomoji_face_pattern.sub('', str(text))
+
+def removing_reddit_pattern(text): # Other noises
+    reddit_pattern = re.compile(r"(\/r\/\w+|u\/\w+|post:)", re.IGNORECASE)
+
+    return reddit_pattern.sub('', str(text))
+
+def cleaning_text(text):
     text = str(text).strip()
     text = deleting_quotes(text)
     text = removing_emojis(text)
+    text = removing_kaoemoji_patterns(text)
+    text = removing_reddit_pattern(text)
     text = re.sub(r'\s+', ' ', text)
-    return text"""
+
+    return text
 
 # Applying the custom functions on the dataset
-def clean_dataset(dataset):
+def cleaning_dataset():
+    dataset = polars.read_csv('datasets/dad-jokes.csv')
+    print(f'Original Shape: {dataset.shape}')
 
     # Checking and removing rows with NaN and Null values
     print('\nNull Count ...\n', dataset.null_count())
@@ -60,12 +81,46 @@ def clean_dataset(dataset):
     
     # Filtering out URLs
     dataset = dataset.filter(~dataset.map_rows(checking_for_url).to_series())
+    print(f'\nAfter filterign out URLs, Dataset Shape: {dataset.shape}')
 
-    
-    
+    # Filtering out HTML Entries
+    dataset = dataset.filter(~dataset.map_rows(checking_for_html_entities).to_series())
+    print(f'\nAfter filterign out HTML entries, Dataset Shape: {dataset.shape}')
 
+    # Filtering out Square Brackets
+    dataset = dataset.filter(~dataset.map_rows(checking_for_square_brackets).to_series())
+    print(f'\nAfter filterign out Square brackets, Dataset Shape: {dataset.shape}')
 
-dataset = polars.read_csv('datasets/dad-jokes.csv')
-print(f'Original Shape: {dataset.shape}')
+    # Deleting quotes, emojis and reddit patterns
+    dataset = dataset.with_columns(
+        (
+            polars.col('question').
+            map_elements(
+                function=lambda t: cleaning_text(t),
+                return_dtype=polars.Utf8
+            ).alias('question')
+        ),
+        (
+            polars.col('response').
+            map_elements(
+                function=lambda t: cleaning_text(t),
+                return_dtype=polars.Utf8
+            ).alias('response')
+        )
+    )
 
-clean_dataset(dataset)
+    # Filtering out texts with small length
+    # NOT considering text with length <= 5 as joke setup questions
+    dataset = dataset.filter(~(polars.col('question').len().is_between(1, 5, 'both')))
+    print(f'\nAfter filterign out short texts, Dataset Shape: {dataset.shape}')
+
+    # Formatting texts for GPT-2 model
+    dataset = dataset.with_columns(
+        ('Q: ' + polars.col('question') + '\nA: ' + polars.col('response') + '\n<|endoftext|>').
+        alias('jokes')
+    )
+    print('\nFinal dataset after formatting ... \n')
+    print(dataset.head())
+
+    dataset.write_csv('datasets/formatted-dad-jokes.csv')
+    print('Formatted dataset saved at file path datasets/ !')
