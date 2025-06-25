d3bb236965bc952f0127b59ad9ca77abf7f8eb4f
Avik Chakraborty <manikchakrabarti745@gmail.com>
2025-06-25 21:47:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src import *
 
-print('Downloading Dataset from HuggingFace ... ')
+"""print('Downloading Dataset from HuggingFace ... ')
 download_dataset()
 
-print('Creating the raw full dataset ...')
+print('\n\nCreating the raw full dataset ...')
 full_raw_dataset_creation()
+
+print('\n\nPreprocessing the dataset ...')
+cleaning_dataset()"""
