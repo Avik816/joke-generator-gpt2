e26fb9797d1f6b1660fea8860d73d52e3abc69bf
Avik816 <manikchakrabarti745@gmail.com>
2025-06-26 03:21:05 +0530

    Create files for model dataset

---
+++ src/data_for_model/model_data_1.py
@@ -0,0 +1,25 @@
+import polars
+from .. import CONFIG
+
+
+def write_into_file(dataset, filename):
+    sample_list = [samples + '\n\n' for samples in dataset['jokes']]
+
+    with open(file=f'data/jokes-{filename}.txt', mode='w', encoding='UTF-8') as fp:
+        fp.writelines(sample_list)
+
+def data_splitting():
+    dataset = polars.read_csv('datasets/formatted-dad-jokes.csv')
+
+    train = dataset[:int(dataset.shape[0] * CONFIG.TRAIN_SIZE), :]
+    val = dataset[int(train.shape[0]):int(train.shape[0] + int(dataset.shape[0] * CONFIG.VAL_SIZE)), :]
+    test = dataset[int(val.shape[0]):int(val.shape[0] + int(dataset.shape[0] * CONFIG.TEST_SIZE)), :]
+
+    write_into_file(train, 'train')
+    print('Training file saved in data/ !')
+
+    write_into_file(val, 'val')
+    print('Validation file saved in data/ !')
+    
+    write_into_file(test, 'test')
+    print('Test file saved in data/ !')
