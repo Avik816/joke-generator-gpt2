5115ec52c487d6e3cf083edae4fdef8ec85c64cc
Avik816 <manikchakrabarti745@gmail.com>
2025-07-03 23:58:34 +0530

    Update model learning curve

--- src/plotting/model_learning_curve.py
+++ src/plotting/model_learning_curve.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as mplot
+from ..CONFIG import OUTPUT_DIR_PLOT
 
-# Plot training vs validation loss
-mplot.plot(history.history['loss'], label='Training Loss')
-mplot.plot(history.history['val_loss'], label='Validation Loss')
-mplot.title("Loss Curve")
-mplot.xlabel("Epochs")
-mplot.ylabel("Loss")
-mplot.legend()
-mplot.grid(True)
-mplot.tight_layout()
-mplot.show()
+
+def plot_learning_curve(history):
+        
+    # Plotting training vs validation loss
+    mplot.plot(history.history['loss'], label='Training Loss')
+    mplot.plot(history.history['val_loss'], label='Validation Loss')
+    mplot.title('Loss Curve')
+    mplot.xlabel('Epochs')
+    mplot.ylabel('Loss')
+    mplot.legend()
+    mplot.grid(True)
+    mplot.tight_layout()
+    
+    # Saving the file
+    mplot.savefig(f'{OUTPUT_DIR_PLOT}/Model learning curve_loss-plot.png')
+    print(f'Plot is saved at {OUTPUT_DIR_PLOT} !')
