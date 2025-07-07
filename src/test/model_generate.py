7b9fd755dccf5a936d890a1becc720b19c4ea384
Avik816 <manikchakrabarti745@gmail.com>
2025-07-07 23:53:42 +0530

    Update generate code

--- src/test/model_generate.py
+++ src/test/model_generate.py
@@ -3,16 +3,16 @@ from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
 from .. import CONFIG
 
 
-# Loading the model
-tf_model = tf.keras.models.load_model('', compile=False) # model path to be added
-hf_model = TFGPT2LMHeadModel.from_pretrained(CONFIG.MODEL_NAME)
-hf_model.set_weights(tf_model.get_weights())
-
-# Loading the tokenizer
-tokenizer = GPT2Tokenizer.from_pretrained(CONFIG.MODEL_NAME)
-tokenizer.pad_token = tokenizer.eos_token
-
 def generate_joke():
+    # Loading the model
+    tf_model = tf.keras.models.load_model('', compile=False) # model path to be added
+    hf_model = TFGPT2LMHeadModel.from_pretrained(CONFIG.MODEL_NAME)
+    hf_model.set_weights(tf_model.get_weights())
+
+    # Loading the tokenizer
+    tokenizer = GPT2Tokenizer.from_pretrained(CONFIG.MODEL_NAME)
+    tokenizer.pad_token = tokenizer.eos_token
+
     input_ids = tokenizer.encode('', return_tensors='tf')
                                  
     output = hf_model.generate(
