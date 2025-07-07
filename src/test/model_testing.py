5d2bf8c8f400d7c9bfa29d4956231fa330182616
Avik816 <manikchakrabarti745@gmail.com>
2025-07-07 23:53:08 +0530

    Update test code

--- src/test/model_testing.py
+++ src/test/model_testing.py
@@ -4,15 +4,16 @@ from ..data_for_model.model_data_2 import load_dataset
 import math
 
 
-# Loading model and tokenizer
-model = tf.keras.models.load_model() # Model path to be added later
+def model_testing_evaluation():
+    # Loading model and tokenizer
+    model = tf.keras.models.load_model() # Model path to be added later
 
-# Test Set data
-test_set = load_dataset(CONFIG.TEST_PATH, False)
+    # Test Set data
+    test_set = load_dataset(CONFIG.TEST_PATH, False)
 
-# Evaluate on test set
-eval_loss = model.evaluate(test_set)
-print(f'\nTest loss: {eval_loss:.4f}')
+    # Evaluate on test set
+    eval_loss = model.evaluate(test_set)
+    print(f'\nTest loss: {eval_loss:.4f}')
 
-# Perplexity Score: Describes how the model predicts the next token
-print(f'The Perplexity Score: {math.exp(eval_loss)}')
+    # Perplexity Score: Describes how the model predicts the next token
+    print(f'The Perplexity Score: {math.exp(eval_loss)}')
