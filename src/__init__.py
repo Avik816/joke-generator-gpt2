9465f0d078495ad5f44193e0c80b106539d71fc5
Avik816 <manikchakrabarti745@gmail.com>
2025-07-07 23:54:28 +0530

    Update files as modules

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,8 @@ from .preprocessing.data_preprocessing import cleaning_dataset
 from .data_for_model.model_data_1 import data_splitting
 #from .data_for_model.model_data_2 import create_model_dataset
 from .joke_trainer.train import training_gpt2_small
+from .test.model_testing import model_testing_evaluation
+from .test.model_generate import generate_joke
 
 #__sub_modules__ = ['download_dataset', 'full_raw_dataset_creation', 'cleaning_dataset', 'data_splitting', 'training_gpt2_small']
 
