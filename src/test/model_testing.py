162faccb2b7d6e964880f59272c3e4ecc0c8053b
Avik816 <manikchakrabarti745@gmail.com>
2025-07-08 23:53:30 +0530

    Updated model loading code

--- src/test/model_testing.py
+++ src/test/model_testing.py
@@ -1,19 +1,28 @@
 import tensorflow as tf
 from .. import CONFIG
+from transformers import TFGPT2LMHeadModel
 from ..data_for_model.model_data_2 import load_dataset
 import math
 
 
 def model_testing_evaluation():
-    # Loading model and tokenizer
-    model = tf.keras.models.load_model() # Model path to be added later
+    # Loading model
+    model = TFGPT2LMHeadModel.from_pretrained(CONFIG.MODEL_NAME)
+    model.load_weights() # path to best model weights
+
+    # Compiling the model
+    learning_rate = CONFIG.BEST_LR # to be added after training
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    )
 
     # Test Set data
     test_set = load_dataset(CONFIG.TEST_PATH, False)
 
     # Evaluate on test set
-    eval_loss = model.evaluate(test_set)
-    print(f'\nTest loss: {eval_loss:.4f}')
+    eval_loss = model.evaluate(test_set, verbose=1)
+    print(f'\nTest loss: {eval_loss:.6f}')
 
     # Perplexity Score: Describes how the model predicts the next token
     print(f'The Perplexity Score: {math.exp(eval_loss)}')
